7e3a24108fb3e6b61d44e385a158e2a7d565b1e6
Antonius Yonathan <atomicbomber.email@gmail.com>
2018-09-05 01:50:33 +0700

    Now seed a pair of room terms belonging to the same room in the same term;

--- database/seeds/ReportsTableSeeder.php
+++ database/seeds/ReportsTableSeeder.php
@@ -23,16 +23,16 @@ class ReportsTableSeeder extends Seeder
     {
         $faker = \Faker\Factory::create();
 
-        $room_term_grade_groups = DB::table('room_terms')
-            ->select('room_terms.id', 'rooms.grade')
+        $room_terms = DB::table('room_terms')
+            ->select('room_terms.term_id', 'room_terms.id', 'rooms.grade', 'room_terms.even_odd', 'room_terms.room_id')
             ->join('rooms', 'rooms.id', 'room_terms.room_id')
             ->get()
-            ->groupBy('grade');
+            ->groupBy(['term_id', 'grade', 'room_id']);
 
         $student_grade_groups = DB::table('students')
             ->select('students.id', 'students.current_grade')
             ->where('active', 1)
-            ->limit(10)
+            ->limit(40)
             ->get()
             ->groupBy('current_grade')
             ->map(function($student_grade_group) { return $student_grade_group->chunk(5); });
@@ -44,35 +44,34 @@ class ReportsTableSeeder extends Seeder
 
         foreach ($student_grade_groups as $grade => $student_groups) {
             // Only create reports if room terms from this grade exist
-            if (isset($room_term_grade_groups[$grade])) {
-                foreach ($student_groups as $student_group) {
+            $room_terms_per_room = $room_terms->first()->get($grade)->random();
+            if ($room_terms_per_room) {
+                foreach ($room_terms_per_room as $room_term) {
+                    foreach ($student_groups as $student_group) {
+                        foreach ($student_group as $student) {
+                            // Report creation
+                            $report = Report::create([
+                                'room_term_id' => $room_term->id,
+                                'student_id' => $student->id,
+                                'social_attitude_description' => $faker->paragraph(2),
+                                'spiritual_attitude_description' => $faker->paragraph(2),
+                                'absence_sick' => $faker->randomNumber(1),
+                                'absence_permit' => $faker->randomNumber(1),
+                                'absence_unknown' => $faker->randomNumber(1)
+                            ]);
 
-                    // Pick a random room term
-                    $room_term_id = $room_term_grade_groups[$grade][0]->id;
-
-                    foreach ($student_group as $student) {
-                        // Report creation
-                        $report = Report::create([
-                            'room_term_id' => $room_term_id,
-                            'student_id' => $student->id,
-                            'social_attitude_description' => $faker->paragraph(2),
-                            'spiritual_attitude_description' => $faker->paragraph(2),
-                            'absence_sick' => $faker->randomNumber(1),
-                            'absence_permit' => $faker->randomNumber(1),
-                            'absence_unknown' => $faker->randomNumber(1)
-                        ]);
-
-                        // Create course reports for each report
-                        if (isset($course_grade_groups[$grade])) {
-                            foreach ($course_grade_groups[$grade] as $course) {
-                                $course_report = CourseReport::create([
-                                    'course_id' => $course->id,
-                                    'report_id' => $report->id,
-                                    'mid_exam' => $faker->numberBetween(50, 100),
-                                    'final_exam' => $faker->numberBetween(50, 100),
-                                    'knowledge_description' => $faker->paragraph(4),
-                                    'skill_description' => $faker->paragraph(4)
-                                ]);
+                            // Create course reports for each report
+                            if (isset($course_grade_groups[$grade])) {
+                                foreach ($course_grade_groups[$grade] as $course) {
+                                    $course_report = CourseReport::create([
+                                        'course_id' => $course->id,
+                                        'report_id' => $report->id,
+                                        'mid_exam' => $faker->numberBetween(50, 100),
+                                        'final_exam' => $faker->numberBetween(50, 100),
+                                        'knowledge_description' => $faker->paragraph(4),
+                                        'skill_description' => $faker->paragraph(4)
+                                    ]);
+                                }
                             }
                         }
                     }
