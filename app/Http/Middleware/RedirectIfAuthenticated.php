696b291b40c66a75ea2bbeb5223dbeba43934565
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-01-28 06:02:18 +0700

    Fixed a bug.

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,18 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            switch ( auth()->user()->privilege ) {
+                case 'teacher':
+                    return redirect()->route('teacher.management.terms');
+                case 'administrator':
+                    return redirect()->route('terms.index');
+                case 'student':
+                    return redirect()->route('student_access.terms');
+                case 'headmaster':
+                    return redirect()->route('headmaster_access.terms');
+                default:
+                    return redirect('/home');
+            }
         }
 
         return $next($request);
